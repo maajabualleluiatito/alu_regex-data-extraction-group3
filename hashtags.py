11b52e38b17089eb6e0cbe549a4f0c6e873c4556
Eldrige <peldrige8@gmail.com>
2024-09-12 14:20:57 +0200

    feat: get user input

--- hashtags.py
+++ hashtags.py
@@ -5,14 +5,11 @@ def is_valid_hashtag(hashtag):
     pattern = r'^#[a-zA-Z0-9_]'
     return re.match(pattern, hashtag) is not None
 
-# Test Cases
+
+user_input = input("Enter the test value: ")
 
 
-# Example Usage
-hashtag_1 = "hello"  # invalid hashtag
-hashtag_2 = "hello#world"  # Invalid hashtag
-hashtag_3 = "#hello-world"  # valid hashtag
-hashtag_4 = 'hello#hello'  # Invalid hashtag
-
-print(f"{hashtag_3} is valid hashtag: {is_valid_hashtag(hashtag_3)}")
-print(f"{hashtag_4} is valid hashtag: {is_valid_hashtag(hashtag_4)}")
+if is_valid_hashtag(user_input):
+    print(f"{user_input} is valid hashtag: {is_valid_hashtag(user_input)}")
+else:
+    print(f"{is_valid_hashtag(user_input)} : {user_input} is invalid")
